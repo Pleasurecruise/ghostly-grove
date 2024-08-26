c7bb8e8d8492dad05c75682bbd73b4b2bf19ec90
自由的世界人 <3196812536@qq.com>
2024-08-27 01:32:23 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  //默认开启ssr服务渲染
+  ssr: true,
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
